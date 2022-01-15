8b247b0105f154f7dba0ba6aca7d3b87c390cb18
Gamerlots <aarindave@gmail.com>
2022-01-14 20:26:29 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ready_code',
+    version='0.1',
+    license='MIT',
+    author="Aarin Dave",
+    author_email='aarindave@protonmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Gamerlots/ready_code.git',
+    keywords='Ready Code',
+    install_requires=[],
+)
